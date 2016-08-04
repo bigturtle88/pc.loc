b670b36f290b46d052ab14f0f0de52e7d24d2909
Vyacheslav <teodorteodor7@gmail.com>
2016-08-04 12:47:51 +0300

    Create MySQL query

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=pc.loc',
     'username' => 'root',
-    'password' => '123456',
+    'password' => '1234567',
     'charset' => 'utf8',
 ];
--- controllers/DeadlineController.php
+++ controllers/DeadlineController.php
@@ -7,51 +7,50 @@ use yii\rest\ActiveController;
 use app\models\Deadline;
 use yii\web\Response;
 
-class DeadlineController extends ActiveController
-{
- 
-  public $modelClass = 'app\models\Deadline';
+class DeadlineController extends ActiveController {
 
-  public function behaviors() {
-    return [
-      [
-        'class' => \yii\filters\ContentNegotiator::className(),
-       'only' => ['index', 'view', 'create', 'update', 'delete'],
-     
-        'formats' => [
-          'application/json' => \yii\web\Response::FORMAT_JSON,
-        ],
-      ],
-    ];
-  }
+    public $modelClass = 'app\models\Deadline';
+
+    public function behaviors() {
+        return [
+            [
+                'class' => \yii\filters\ContentNegotiator::className(),
+                'only' => ['index', 'view', 'create', 'update', 'delete'],
+                'formats' => [
+                    'application/json' => \yii\web\Response::FORMAT_JSON,
+                ],
+            ],
+        ];
+    }
+
     public function actions() {
-    $actions = parent::actions();
-    unset($actions['index']);
-    return $actions;
-  }
+        $actions = parent::actions();
+        unset($actions['index']);
+        return $actions;
+    }
 
-  public function actionIndex() {
-    
-    $query =  new \yii\db\Query();
-    $subQuery = new \yii\db\Query();
-    
-    $comments_count = $subQuery->select('COUNT(comments.deadline_id )')
-      ->where(['comments.deadline_id' =>  'deadline.id'])
-      ->from('comments');
-    
-    $task = $query->select(['deadline.id','deadline.text','deadline.status','deadline.deadline_date','comments_count' => $comments_count])
-      ->where(['deadline.status' =>  1])
-      ->from('deadline')
-      ->all(); 
-    
-  //  $task = $query->createCommand()->sql;var_dump($task);die();
-    
-    $taskUncheked = $query->select(['deadline.id','deadline.text','deadline.status','deadline.deadline_date','comments_count' => $comments_count])
-      ->where(['deadline.status' =>  '0'])
-      ->from('deadline')
-      ->orderBy(['deadline.deadline_date' => SORT_ASC])
-      ->all();
-        
-    return    $task + $taskUncheked;
-  }
+    public function actionIndex() {
+
+        $query1 = new \yii\db\Query();
+  $query2 = new \yii\db\Query();
+
+        $task = $query1->select(['deadline.id', 'deadline.text', 'deadline.status', 'deadline.deadline_date', 'COUNT(`comments`.`deadline_id`) AS comments_count'])
+                ->from('deadline')
+                ->join('LEFT JOIN', 'comments', '`deadline`.`id` = `comments`.`deadline_id`')
+                ->where('`deadline`.`status` = 1')
+                ->groupBy('deadline.id')
+                ->all();
+
+
+   
+         $taskUncheked = $query2->select(['deadline.id', 'deadline.text', 'deadline.status', 'deadline.deadline_date', 'COUNT(`comments`.`deadline_id`) AS comments_count'])
+                ->from('deadline')
+                ->join('LEFT JOIN', 'comments', '`deadline`.`id` = `comments`.`deadline_id`')
+                ->where('`deadline`.`status` = 0')
+                ->groupBy('`deadline`.`id`')
+                ->all();
+
+        return $task + $taskUncheked;
+    }
+
 }
